b1981b5e7399d9ce2733e622ca9aa9ace126e649
MihirThumar <mihirthumaraaa@gmail.com>
2023-02-10 20:17:41 +0530

    First

--- springbootcrud/src/main/java/com/crud/SpringBootCrudApplication.java
+++ springbootcrud/src/main/java/com/crud/SpringBootCrudApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCrudApplication {
+public class SpringBootCrudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCrudApplication.class, args);
+		SpringApplication.run(SpringBootApplication.class, args);
 	}
 
 }
